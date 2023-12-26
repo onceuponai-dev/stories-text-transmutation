b13d9153981d37c089e5904cad83d630f5100dac
qooba <dev@qooba.net>
2023-12-26 19:38:38 +0100

    add image

---
+++ src/build/m_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_model_free(a: number): void;
+export function model_load(a: number, b: number, c: number, d: number, e: number, f: number, g: number): void;
+export function model_forward(a: number, b: number, c: number): void;
+export function main(a: number, b: number): number;
+export function greet(): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__hbb6a92f07c74d8f3(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h750c36762897e2d7(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_start(): void;
